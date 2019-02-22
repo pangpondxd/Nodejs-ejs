2b355b7597233eba5a5fec1be5a7221ad6531b45
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-02-22 17:10:36 +0700

    Add files via upload

--- indexlogin.js
+++ indexlogin.js
@@ -2,34 +2,30 @@ var express = require('express')
 var session = require('express-session')
 var bodyParser = require('body-parser')
 var app = express()
-
+var urlencodedParser = bodyParser.urlencoded({ extend: false})
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000}, resave : false, saveUninitialized:false}))
-app.use(function(req, res, next) {
+app.get('/', function(req, res) {
+   res.render('index')
+})
+app.get('/admin', function(req, res, next) {
     var sess = req.session
     if(sess.views){
         sess.views++
+        res.render('')
     }else{
         sess.views = 1
+        res.render('admin')
     }
     console.log(sess.views)
     next();
 })
-app.get('/', function(req, res) {
-   res.send('Hello XX')
-})
-app.get('./admin', function(req, res) {
-    var sess = req.session
-    if(sess.views){
-        res.render('Hello')
-    }else{
-        sess.views = 1
-        res.render('Please log in')
-    }
-    console.log(sess.views)
-    next();
+
+app.post('/add', urlencodedParser, function(req, res) {
+    var result = parseInt(req.body.a) + parseInt(req.body.b)
+    res.send('Result = ' + result)
 })
 
 app.listen(8000)
